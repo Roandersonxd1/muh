08b802d9c74a54b4c493cc172219cda2279b7bd1
Roanderson Pinheiro <roandersonfelipe@gmail.com>
2021-01-13 12:46:46 -0300

    Fix ktlint

--- android/app/src/main/java/com/example/myos/ui/CreateAccountActivity.kt
+++ android/app/src/main/java/com/example/myos/ui/CreateAccountActivity.kt
@@ -1,7 +1,7 @@
 package com.example.myos.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.myos.R
 
 class CreateAccountActivity : AppCompatActivity() {
