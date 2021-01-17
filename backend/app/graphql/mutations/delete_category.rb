0e7a62281f92b99aef17eb307375c2e9cdaf47bb
Edimo Sousa <edimossilva@gmail.com>
2021-01-17 12:56:30 +0200

    ✨ delete category

---
+++ backend/app/graphql/mutations/delete_category.rb
@@ -0,0 +1,14 @@
+module Mutations
+  class DeleteCategory < BaseMutation
+    argument :id, ID, required: true
+    type Types::CategoryType
+
+    def resolve(id: nil)
+      category = Category.find_by!(id: id)
+      category.destroy!
+      category
+    rescue ActiveRecord::RecordNotFound => _e
+      GraphQL::ExecutionError.new("Category not found with id #{id}.")
+    end
+  end
+end
