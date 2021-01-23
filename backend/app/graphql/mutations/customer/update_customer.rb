b65dd4c80153d96869223fd461a3a3f9a8f3dcea
Edimo Sousa <edimossilva@gmail.com>
2021-01-23 12:21:52 +0200

    refactor update mutations

--- backend/app/graphql/mutations/customer/update_customer.rb
+++ backend/app/graphql/mutations/customer/update_customer.rb
@@ -1,21 +1,12 @@
 module Mutations::Customer
-  class UpdateCustomer < Mutations::BaseMutation
-    argument :id, ID, required: true
+  class UpdateCustomer < Mutations::Generic::UpdateMutation
+    self.resource_class = Customer
 
+    argument :id, ID, required: true
     argument :name, String, required: true
     argument :email, String, required: true
     argument :phone, GraphQL::Types::BigInt, required: true
 
     type Types::Model::CustomerType
-
-    def resolve(id:, name:, phone:, email:)
-      category = Customer.find_by!(id: id, user: current_user)
-      category.update!(
-        name: name,
-        phone: phone,
-        email: email
-      )
-      category
-    end
   end
 end
